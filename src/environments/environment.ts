7736e9225e7a2e2ba8b1a050eb57ca1eb456e370
Aldo <acitalan@buhoms.com>
2026-05-12 19:07:06 -0600

    fix: update receipt print and reprint URLs in environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:3000/v1',
-  receiptPrintUrl: 'http://localhost/index.php',
-  receiptReprintUrl: 'http://localhost/reprint.php'
+  receiptPrintUrl: 'http://localhost/ticket-api/index.php',
+  receiptReprintUrl: 'http://localhost/ticket-api/reprint.php'
 };
